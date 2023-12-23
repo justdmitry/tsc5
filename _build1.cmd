108d53a9412e9eb675dfe0dae7e767bf61bade87
Dmitry Popov <dmitry@parsec-club.ru>
2023-12-24 00:19:02 +0300

    _build1 and 2

--- _build1.cmd
+++ _build1.cmd
@@ -1,1 +1,2 @@
-npx func-js ./contracts/task1.fc --boc ./build/task1.boc --fift ./build/task1.fift
+@echo off
+npx blueprint build Task1 && npx func-js ./contracts/task1.fc --boc ./build/task1.boc --fift ./build/task1.fift
---
+++ _build2.cmd
@@ -0,0 +1,2 @@
+@echo off
+npx blueprint build Task2 && npx func-js ./contracts/task2.fc --boc ./build/task2.boc --fift ./build/task2.fift
