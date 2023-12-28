8910997a6561e03919ee04b2d81489580df8e180
Dmitry Popov <dmitry@parsec-club.ru>
2023-12-28 10:59:40 +0300

    Task 4: opt ~replace

--- _build4.cmd
+++ _build4.cmd
@@ -1,2 +1,2 @@
 @echo off
-npx blueprint build Task4 && npx func-js ./contracts/task4.fc --boc ./build/task4.boc --fift ./build/task4.fift
+npx blueprint build Task4 && npx func-js ./contracts/task4.fc --boc ./build/task4.boc --fift ./build/task4.fift && npx blueprint test
